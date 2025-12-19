5345ff90052b6e6f4485db438924e35f95ed1ee1
KojoTello <papatello321@gmail.com>
2025-12-19 01:08:14 +0000

    Implement dynamic MySQL connection handling
    Add support for MySQL connection via environment variable.

--- db.php
+++ db.php
@@ -1,10 +1,18 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "ashesi_review_db";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+if (getenv('MYSQL_URL')) {
+    $url = parse_url(getenv('MYSQL_URL'));
+    $servername = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $dbname = substr($url["path"], 1);
+    $port = $url["port"];
+    
+    $conn = new mysqli($servername, $username, $password, $dbname, $port);
+} else {
+  
+    $conn = new mysqli("localhost", "root", "", "ashesi_review_db");
+}
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
